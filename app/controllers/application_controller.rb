fed536c9223249bfb6878cfae16cfdbcb966f0a1
ry-kama12 <ryka11223344@gmail.com>
2020-01-29 17:17:33 +0900

    finish-fanction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
+
+  def after_sign_in_path_for(resource)
+    root_path # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    '/users/sign_in' # サインアウト後のリダイレクト先URL
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
+
+  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    # devise_scope :uset do
+    #   get 'login', to: 'devise/sessions#new'
+    #   post 'login', to: 'devise/sessions#create'
+    #   delete 'signout', to: 'devise/sessions#destroy'
+    # end
 end
